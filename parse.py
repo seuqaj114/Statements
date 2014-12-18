d80c2f300cb498a2c65ef7c05659eb6f6392a229
seuqaj114 <migjacques@hotmail.com>
2014-12-18 15:05:48 +0000

    File to clean statements!

---
+++ parse.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import sys
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+def clean_text(string):
+	striped = [c for c in string if 0<ord(c)<127]
+	return "".join(striped)
+
+client = MongoClient("mongodb://admin:admin@kahana.mongohq.com:10009/courier_db")
+db = client.courier_db
+
+for item in db.statements.find():
+	print item["subject"]
+	db.statements.update({"_id":ObjectId(item["_id"])},{"$set":{"text":clean_text(item["text"])}})
+
+print "Parsing complete!"
